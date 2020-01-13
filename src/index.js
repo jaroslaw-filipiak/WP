f4d771ecca2ba7d0622c21263beee1e9239c4fd5
Jaroslaw Filipiak <info@j-filipiak.pl>
2020-01-13 14:10:44 +0100

    homepage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import 'bootstrap';
-import './sass/main.scss';
-import './../node_modules/slick-carousel/slick/slick.scss';
-import './../node_modules/slicknav/scss/slicknav.scss';
-import './../node_modules/slicknav/scss/core.scss';
-import './../node_modules/slicknav/scss/theme.scss';
-import './../node_modules/aos/src/sass/aos.scss';
+//import 'bootstrap'
+import './sass/main.scss'
+import './../node_modules/slick-carousel/slick/slick.scss'
+import './../node_modules/aos/src/sass/aos.scss'
 
-const $ = require('jquery');
-const slick = require('slick-carousel');
-const slicknav = require('slicknav/jquery.slicknav');
-const waypoints = require('././../node_modules/waypoints/lib/jquery.waypoints');
-const Parallax = require('parallax-js');
-const AOS = require('aos');
+const $ = require('jquery')
+const slick = require('slick-carousel')
+const slicknav = require('slicknav/jquery.slicknav')
+//const waypoints = require('././../node_modules/waypoints/lib/jquery.waypoints');
+//const Parallax = require('parallax-js');
+//const AOS = require('aos');
+
+const logoMobile = require('./assets/logo.png');
 
 //AOS.init();
+
+$('#menu').slicknav({
+   duplicate: true,
+   label: '',
+   brand: `<img class="logo-mobile" src="${logoMobile}">`,
+   init: function () {
+      // $('a.slicknav_btn').append(`<img src="img/search.svg" alt="search">`)
+   },
+})
+
+if (document.querySelector('.hero-slider')) {
+   $('.hero-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: true,
+      speed: 200,
+      prevArrow: $('.hero-slider-nav-prev'),
+      nextArrow: $('.hero-slider-nav-next'),
+   })
+}
